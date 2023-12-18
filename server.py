84eec6c2310273f48e623447b5fff1909e46c2de
João Chaves <joao.chaves@traivefinance.com>
2023-12-18 15:26:41 -0300

    feat: server built

---
+++ server.py
@@ -0,0 +1,97 @@
+import socket
+import signal
+import sys
+import os
+from utils import *
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+proxy_port = int(os.getenv('PROXY_PORT'))
+proxy_host = os.getenv('PROXY_HOST')
+
+def send_server_up_notification():
+    send_message(
+        "SERVER UP",
+        proxy_host,
+        proxy_port
+    )
+
+def handle_shutdown(host, port):
+    print(f'Server at {host}:{port} shutting down')
+    send_message(
+        "SERVER DOWN",
+        proxy_host,
+        proxy_port
+    )
+
+def store_file(client_socket, filename):
+    with open(filename, 'wb') as file:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+
+def retrieve_file(filename):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((proxy_host, proxy_port))
+
+    with open(filename, 'rb') as file:
+        data = file.read(1024)
+        while data:
+            client_socket.send(data)
+            data = file.read(1024)
+
+    client_socket.close()
+
+
+def process_message(client_socket):
+    message = client_socket.recv(1024).decode('utf-8')
+
+    if message.startswith("RECOVER"):
+        print("RECOVER message received")
+        filename = message.split(' ')[1].split('.')[0]
+
+        retrieve_file(filename+"-processed.txt")
+        print(f"Recovering file: {filename}")
+        print("\nRelaying file to proxy...")
+
+    elif message.startswith("DELETE"):
+        filename = message.split(" ")[1]
+        if os.path.exists(filename):
+            os.remove(filename)
+            print(f"The file '{filename}' has been deleted.")
+        else:
+            print(f"The file '{filename}' does not exist on this server.")
+    elif message.startswith("STORE"):
+        filename = message.split(" ")[1]
+        if not os.path.exists(filename):
+            store_file(client_socket, filename)
+        else:
+            print(f'File {filename} already exists in this server')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+
+        if arg.startswith("PORT="):
+            port = int(arg.split("=")[1])
+            server_socket = start_server(port)
+            send_server_up_notification()
+        else:
+            print("Error: Wrong argument. Use the format 'PORT=port_number'.")
+    else:
+        print("Error: No given argument.  Use the format 'PORT=port_number'.")
+
+    signal.signal(signal.SIGTERM, handle_shutdown)
+    signal.signal(signal.SIGINT, handle_shutdown)
+
+    print("Server recognized by proxy")
+    while True:
+        print("waiting message")
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
+        process_message(client_socket)
