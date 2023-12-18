131c97595b50574fdfdecdd08170b007742bea2d
João Chaves <joao.chaves@traivefinance.com>
2023-12-18 12:13:19 -0300

    feat: chore project structure created

---
+++ utils.py
@@ -0,0 +1,23 @@
+import socket
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def start_server(port: int):
+    host = os.getenv('SERVER_HOST')
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)  
+
+    print(f"Up and running on {host}:{port}")
+
+    return server_socket
+
+
+def send_message(message, address, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((address, port))
+    client_socket.send(message.encode('utf-8'))
+    client_socket.close()
